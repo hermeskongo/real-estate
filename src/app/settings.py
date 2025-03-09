48b1c3e5e7760bd80b914e9358b61941b9d8cb51
hermeskongo <hermeskongo@gmail.com>
2025-03-09 00:33:31 +0000

    Configurations pour le déploiement

--- src/app/settings.py
+++ src/app/settings.py
@@ -13,18 +13,25 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from environ import environ
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(str(BASE_DIR/"app"/".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1ehk58=u)!ncrrnd^3hujr-yo(v^5lt7ra8-2p2#ua7s#tt%uf'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+print(ALLOWED_HOSTS)
 
 # Application definition
 
